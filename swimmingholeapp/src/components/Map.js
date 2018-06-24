178e1ba32f241d16e05696569c5f7e0a0dd33c99
Nathan Simmons <nathanhbsimmons@gmail.com>
2018-06-24 14:35:13 -0500

    changed map image

--- swimmingholeapp/src/components/Map.js
+++ swimmingholeapp/src/components/Map.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import map from '../img/mapPicture.png'
+import map from '../img/mapZoom.png'
 import '../App.css';
 import {filterSelectedSite} from '../Scripts';
 import ImageMap from 'image-map'
@@ -15,16 +15,16 @@ class ClickMap extends Component {
         render(props){
             return (
                 <div>
-                    <img className="mapImage" src={map} alt="" useMap="#image-map" />
-                    <map name="image-map" id="Map">
-                        <area onClick={(event)=>{this.props.setValue(Number(event.target.id)); this.props.setDisplaySite(filterSelectedSite(event, null, this.props.sites))}} alt="" id="6" title="Pedernales Falls"  href="#" shape="poly" coords="77,155,76,188,116,187,112,155" />
-                        <area onClick={(event)=>{this.props.setValue(Number(event.target.id)); this.props.setDisplaySite(filterSelectedSite(event, null, this.props.sites))}} alt="" id="2" title="Bull Creek" href="#" shape="poly" coords="784,79,785,118,820,117,817,80" />
-                        <area onClick={(event)=>{this.props.setValue(Number(event.target.id)); this.props.setDisplaySite(filterSelectedSite(event, null, this.props.sites))}} alt="" id="4" title="Lost Creek" href="#" shape="poly" coords="676,267,678,303,712,304,711,267" />
-                        <area onClick={(event)=>{this.props.setValue(Number(event.target.id)); this.props.setDisplaySite(filterSelectedSite(event, null, this.props.sites))}} alt="" id="5" title="Barton Springs" href="#" shape="poly" coords="778,285,781,317,812,317,812,284" />
-                        <area onClick={(event)=>{this.props.setValue(Number(event.target.id)); this.props.setDisplaySite(filterSelectedSite(event, null, this.props.sites))}} alt="" id="3" title="Gus Fruh" href="#" shape="poly" coords="743,315,744,352,775,350,772,316" />
-                        <area onClick={(event)=>{this.props.setValue(Number(event.target.id)); this.props.setDisplaySite(filterSelectedSite(event, null, this.props.sites))}} alt="" id="7" title="Camp Ben McCulloch" href="#" shape="poly" coords="429,510,431,550,463,550,463,511" />
-                        <area onClick={(event)=>{this.props.setValue(Number(event.target.id)); this.props.setDisplaySite(filterSelectedSite(event, null, this.props.sites))}} alt="" id="8" title="Jacob's Well" href="#" shape="poly" coords="269,670,272,706,302,706,298,669" />
-                        <area onClick={(event)=>{this.props.setValue(Number(event.target.id)); this.props.setDisplaySite(filterSelectedSite(event, null, this.props.sites))}} alt="" id="9" title="Blue Hole" href="#" shape="poly" coords="322,720,324,759,352,760,354,724" />
+                    <img className="mapImage" src={map} alt="" useMap="#image-map" id="map-image" />
+                    <map name="image-map" >
+                        <area onClick={(event)=>{this.props.setValue(Number(event.target.id)); this.props.setDisplaySite(filterSelectedSite(event, null, this.props.sites))}} alt="" id="6" title="Pedernales Falls"  href="#" shape="circle"   coords="30, 68, 14" />
+                        <area onClick={(event)=>{this.props.setValue(Number(event.target.id)); this.props.setDisplaySite(filterSelectedSite(event, null, this.props.sites))}} alt="" id="2" title="Bull Creek" href="#" shape="circle" coords="383, 31, 14" />
+                        <area onClick={(event)=>{this.props.setValue(Number(event.target.id)); this.props.setDisplaySite(filterSelectedSite(event, null, this.props.sites))}} alt="" id="4" title="Lost Creek" href="#" shape="circle" coords="329, 124, 16" />
+                        <area onClick={(event)=>{this.props.setValue(Number(event.target.id)); this.props.setDisplaySite(filterSelectedSite(event, null, this.props.sites))}} alt="" id="5" title="Barton Springs" href="#" shape="circle" coords="380, 132, 14" />
+                        <area onClick={(event)=>{this.props.setValue(Number(event.target.id)); this.props.setDisplaySite(filterSelectedSite(event, null, this.props.sites))}} alt="" id="3" title="Gus Fruh" href="#" shape="circle" coords="361, 148, 14" />
+                        <area onClick={(event)=>{this.props.setValue(Number(event.target.id)); this.props.setDisplaySite(filterSelectedSite(event, null, this.props.sites))}} alt="" id="7" title="Camp Ben McCulloch" href="#" shape="circle" coords="205, 247, 16" />
+                        <area onClick={(event)=>{this.props.setValue(Number(event.target.id)); this.props.setDisplaySite(filterSelectedSite(event, null, this.props.sites))}} alt="" id="8" title="Jacob's Well" href="#" shape="circle" coords="124, 325, 14"  />
+                        <area onClick={(event)=>{this.props.setValue(Number(event.target.id)); this.props.setDisplaySite(filterSelectedSite(event, null, this.props.sites))}} alt="" id="9" title="Blue Hole" href="#" shape="circle" coords="150, 351, 15" />
                     </map>
                 </div>
             );
