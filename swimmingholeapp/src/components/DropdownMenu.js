cf9726b4a09586f1eaaa2b38b32640bab5a045e5
Nathan Simmons <nathanhbsimmons@gmail.com>
2018-07-06 15:18:43 -0500

    added dropdown bug fix

--- swimmingholeapp/src/components/DropdownMenu.js
+++ swimmingholeapp/src/components/DropdownMenu.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import '../App.css';
 import DropDownMenu from 'material-ui/DropDownMenu';
 import MenuItem from 'material-ui/MenuItem';
@@ -7,35 +7,51 @@ import {filterSelectedSite} from '../Scripts'
 
 
 
-function DropdownMenu(props) {
-  const styles = {
-    color: "rgba(70,130,180,0.93)",
-    fontFamily: "'Josefin Sans', sans-serif",
-    fontWeight: "200",
-    contrastText: "rgba(70,130,180,0.93)",
-  }
-    
-  return (
-    <div   className="dropdownMenuDiv" >
-      <DropDownMenu
-      style={styles}
-        className="dropdownMenu"
-        value={props.value}
-        onChange={(event, index, value)=>{props.setValue(value); props.setDisplaySite(filterSelectedSite(event, index, props.sites))}}
-        autoWidth={true}
-      >
-        <MenuItem  style={styles} className="menuItem"  value={1} primaryText="Choose a spot" />
-        <MenuItem  style={styles} className="menuItem"  value={2} onChange={(event, index, value)=>{props.setValue(value); props.setDisplaySite(filterSelectedSite(event, index, props.sites))}} primaryText="Bull Creek @ Loop 360" />
-        <MenuItem  style={styles} className="menuItem"  value={3} onChange={(event, index, value)=>{props.setValue(value); props.setDisplaySite(filterSelectedSite(event, index, props.sites))}} primaryText="Gus Fruh (Greenbelt)" />
-        <MenuItem  style={styles} className="menuItem"  value={4} onChange={(event, index, value)=>{props.setValue(value); props.setDisplaySite(filterSelectedSite(event, index, props.sites))}}  primaryText="Lost Creek (Greenbelt)" />
-        <MenuItem  style={styles} className="menuItem"  value={5} onChange={(event, index, value)=>{props.setValue(value); props.setDisplaySite(filterSelectedSite(event, index, props.sites))}}  primaryText="Barton Springs (Upper)" />
-        <MenuItem  style={styles} className="menuItem"  value={6} onChange={(event, index, value)=>{props.setValue(value); props.setDisplaySite(filterSelectedSite(event, index, props.sites))}}  primaryText="Pedernales Falls" />
-        <MenuItem  style={styles} className="menuItem"  value={7} onChange={(event, index, value)=>{props.setValue(value); props.setDisplaySite(filterSelectedSite(event, index, props.sites))}}  primaryText="Camp Ben McColough" />
-        <MenuItem  style={styles} className="menuItem"  value={8} onChange={(event, index, value)=>{props.setValue(value); props.setDisplaySite(filterSelectedSite(event, index, props.sites))}}  primaryText="Jacob's Well" />
-        <MenuItem  style={styles} className="menuItem"  value={9} onChange={(event, index, value)=>{props.setValue(value); props.setDisplaySite(filterSelectedSite(event, index, props.sites))}}  primaryText="Blue Hole" />
-      </DropDownMenu>
-    </div>
-  );
+class DropdownMenu extends Component {
+  
+
+  
+handleChange=(event, index, value, props)=>{
+       
+    if(this.props.sites.length > 0){
+     return (this.props.setValue(value), this.props.setDisplaySite(filterSelectedSite(event, index, this.props.sites)))
+     
+    } else {
+        return
+    }
+         
+ }    
+   render(props) {
+    const styles = {
+      color: "rgba(70,130,180,0.93)",
+      fontFamily: "'Josefin Sans', sans-serif",
+      fontWeight: "200",
+      contrastText: "rgba(70,130,180,0.93)",
+    }
+
+    return (
+      <div   className="dropdownMenuDiv" >
+        <DropDownMenu
+        style={styles}
+          className="dropdownMenu"
+          value={this.props.value}
+          onChange={(event, index, value)=>{this.handleChange(event, index, value)}}
+          autoWidth={true}
+        >
+          <MenuItem  style={styles} className="menuItem"  value={1} primaryText="Choose a spot" />
+          <MenuItem  style={styles} className="menuItem"  value={2} onChange={(event, index, value)=>{this.handleChange(event, index, value)}} primaryText="Bull Creek @ Loop 360" />
+          <MenuItem  style={styles} className="menuItem"  value={3} onChange={(event, index, value)=>{this.handleChange(event, index, value)}} primaryText="Gus Fruh (Greenbelt)" />
+          <MenuItem  style={styles} className="menuItem"  value={4} onChange={(event, index, value)=>{this.handleChange(event, index, value)}}  primaryText="Lost Creek (Greenbelt)" />
+          <MenuItem  style={styles} className="menuItem"  value={5} onChange={(event, index, value)=>{this.handleChange(event, index, value)}}  primaryText="Barton Springs (Upper)" />
+          <MenuItem  style={styles} className="menuItem"  value={6} onChange={(event, index, value)=>{this.handleChange(event, index, value)}}  primaryText="Pedernales Falls" />
+          <MenuItem  style={styles} className="menuItem"  value={7} onChange={(event, index, value)=>{this.handleChange(event, index, value)}}  primaryText="Camp Ben McColough" />
+          <MenuItem  style={styles} className="menuItem"  value={8} onChange={(event, index, value)=>{this.handleChange(event, index, value)}}  primaryText="Jacob's Well" />
+          <MenuItem  style={styles} className="menuItem"  value={9} onChange={(event, index, value)=>{this.handleChange(event, index, value)}}  primaryText="Blue Hole" />
+        </DropDownMenu>
+      </div>
+    );
+   } 
+  
 
  
    
